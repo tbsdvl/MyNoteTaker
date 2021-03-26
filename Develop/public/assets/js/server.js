079199d66f33c7736e627e8c43d76e0b4cff640a
tbsdvl <tristonburns97@gmail.com>
2021-03-26 16:51:46 -0700

    Testing api connection

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,26 +1,37 @@
-const express = require("../../express");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
-
 
 const PORT = process.env.PORT || 8080;
 
+// const myStructure = [
+//     {
+//         name: "Triston",
+//         profession: "Student"
+//     }
+// ];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
 
-app.get("*", (req, res) => {
-    res.send("index.html");
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.send("notes.html");
+    res.sendFile(path.join(__dirname, "../notes.html"));
 });
 
 app.get("/api/notes", (req, res) => {
-    res.send("db.json");
-});
-
-// For unique ids, i need uuid from npm
-app.post("/api/notes", (req, res) => {
-    res.send("")
+    fs.readFile("../../../db/db.json", (err, data)  => {
+        if (err) throw err;
+        let obj = JSON.parse(data);
+        console.log(obj);
+        res.json(obj);
+    })
 });
 
 app.listen(PORT, console.log(`App listening on PORT: ${PORT}`));

0fa669e0f176b0a4701e62e2e33f2353bf7accaf
tbsdvl <tristonburns97@gmail.com>
2021-03-26 18:55:05 -0700

    Testing public static files

--- Develop/server.js
+++ Develop/server.js
@@ -5,33 +5,33 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// const myStructure = [
-//     {
-//         name: "Triston",
-//         profession: "Student"
-//     }
-// ];
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../notes.html"));
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
 app.get("/api/notes", (req, res) => {
-    fs.readFile("../../../db/db.json", (err, data)  => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data)  => {
         if (err) throw err;
         let obj = JSON.parse(data);
         console.log(obj);
         res.json(obj);
     })
 });
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+
+    fs.appendFile(path.join(__dirname, "/db/db.json"), newNote, (err) => {
+        if (err) throw err;
+    })
+})
 
 app.listen(PORT, console.log(`App listening on PORT: ${PORT}`));
